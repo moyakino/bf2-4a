f76bb01b200815192addddee7606f5e7874cfadc
Ryoya-Kouchi <120348450+Ryoya-Kouchi@users.noreply.github.com>
2023-06-15 11:52:39 +0900

    PAD,FPS,実装

--- bf/FPS.h
+++ bf/FPS.h
@@ -1,1 +1,49 @@
 #pragma once
+#include <math.h>
+#include <chrono>
+#include <thread>
+#include "DxLib.h"
+
+class Fps {
+	int mStartTime;         //測定開始時刻
+	int mCount;             //カウンタ
+	float mFps;             //fps
+	static const int N = 60;//平均を取るサンプル数
+	static const int FPS = 60;	//設定したFPS
+
+public:
+	Fps() {
+		mStartTime = 0;
+		mCount = 0;
+		mFps = 0;
+	}
+
+	bool Update() {
+		if (mCount == 0) { //1フレーム目なら時刻を記憶
+			mStartTime = GetNowCount();
+		}
+		if (mCount == N) { //60フレーム目なら平均を計算する
+			int t = GetNowCount();
+			mFps = 1000.f / ((t - mStartTime) / (float)N);
+			mCount = 0;
+			mStartTime = t;
+		}
+		mCount++;
+		return true;
+	}
+
+	void Draw() {
+		DrawFormatString(0, 0, GetColor(0, 0, 0), "fps : %.1f", mFps);
+	}
+
+	void Wait() {
+		int tookTime = GetNowCount() - mStartTime;	//かかった時間
+		int waitTime = mCount * 1000 / FPS - tookTime;	//待つべき時間
+
+		if (waitTime > 0) {
+			std::chrono::milliseconds dura(waitTime);
+			std::this_thread::sleep_for(dura);
+			//Sleep(waitTime);	//待機
+		}
+	}
+};
--- bf/main.cpp
+++ bf/main.cpp
@@ -1,14 +1,40 @@
 #include "DxLib.h"
+#include "FPS.h"
+#include"SceneManager.h"
+#include"AbstractScene.h"
+#include"PadInput.h"
+#include"FPS.h"
+#define FRAMERATE 60.0 //フレームレート
+
 // プログラムは WinMain から始まります
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int
-	nCmdShow)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow)
 {
-	if (DxLib_Init() == -1) // DXライブラリ初期化処理
-	{
-		return -1; // エラーが起きたら直ちに終了
-	}
+	Fps fps;
+	SetMainWindowText("BAROON FIGHT");
+	ChangeWindowMode(TRUE);
+	if (DxLib_Init() == -1)  return -1; // DXライブラリ初期化処理
+	SetDrawScreen(DX_SCREEN_BACK);	// 描画先画面を裏にする
 	DrawPixel(320, 240, GetColor(255, 255, 255)); // 点を打つ
 	WaitKey(); // キー入力待ち
+	SceneManager* sceneMng;
 	DxLib_End(); // DXライブラリ使用の終了処理
 	return 0; // ソフトの終了
+
+	while ((ProcessMessage() == 0) && (sceneMng->Update() != nullptr))
+	{
+
+		ClearDrawScreen();		// 画面の初期化
+		PAD_INPUT::UpdateKey();
+		sceneMng->Draw();
+		//強制終了
+		if (PAD_INPUT::OnButton(XINPUT_BUTTON_BACK))
+		{
+			break;
+		}
+		fps.Update();	//更新
+		ScreenFlip();			// 裏画面の内容を表画面に反映
+		fps.Wait();		//待機
+	}
+	return 0;
 }
