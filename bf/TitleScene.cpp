6ba93809a472beb23ee61a9e13ffb0c9dabb9a89
moyakino <104491816+moyakino@users.noreply.github.com>
2023-06-22 17:03:12 +0900

    main 修正　titlescene追加

---
+++ bf/TitleScene.cpp
@@ -0,0 +1,13 @@
+#include "DxLib.h"
+#include "TitleScene.h"
+
+//“ü—Í‘¤‚ğ‘‚­
+AbstractScene* Title::Update()
+{
+	return this;
+}
+
+void Title::Draw()const
+{
+	
+}
--- bf/main.cpp
+++ bf/main.cpp
@@ -1,40 +1,60 @@
 #include "DxLib.h"
 #include "FPS.h"
-#include"SceneManager.h"
-#include"AbstractScene.h"
-#include"PadInput.h"
-#include"FPS.h"
+#include "SceneManager.h"
+#include "AbstractScene.h"
+#include "PadInput.h"
+#include "TitleScene.h"
+
 #define FRAMERATE 60.0 //フレームレート
 
 // プログラムは WinMain から始まります
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-	LPSTR lpCmdLine, int nCmdShow)
+int WINAPI WinMain(_In_ HINSTANCE ih, _In_opt_ HINSTANCE ioh, _In_ LPSTR il, _In_ int ii)
 {
-	Fps fps;
+	//タイトル名
 	SetMainWindowText("BAROON FIGHT");
-	ChangeWindowMode(TRUE);
-	if (DxLib_Init() == -1)  return -1; // DXライブラリ初期化処理
-	SetDrawScreen(DX_SCREEN_BACK);	// 描画先画面を裏にする
-	DrawPixel(320, 240, GetColor(255, 255, 255)); // 点を打つ
-	WaitKey(); // キー入力待ち
-	SceneManager* sceneMng;
-	DxLib_End(); // DXライブラリ使用の終了処理
-	return 0; // ソフトの終了
 
-	while ((ProcessMessage() == 0) && (sceneMng->Update() != nullptr))
+	//ウインドウモードで起動
+	ChangeWindowMode(TRUE);
+
+	//画面サイズ指定
+	if(SetGraphMode(640, 480, 32) != DX_CHANGESCREEN_OK)
+	{
+		return -1;
+	}
+
+	// DXライブラリ初期化処理
+	if (DxLib_Init() == -1) 
+	{
+		return -1;
+	}
+		
+	SetDrawScreen(DX_SCREEN_BACK);	// 描画先画面を裏にする
+
+	SceneManager sceneMng(dynamic_cast<AbstractScene*>(new Title()));;
+	Fps fps;
+
+	while ((ProcessMessage() == 0) && (sceneMng.Update() != nullptr))
 	{
 
 		ClearDrawScreen();		// 画面の初期化
 		PAD_INPUT::UpdateKey();
-		sceneMng->Draw();
+		sceneMng.Draw();
 		//強制終了
 		if (PAD_INPUT::OnButton(XINPUT_BUTTON_BACK))
 		{
 			break;
 		}
+
+		//fps
 		fps.Update();	//更新
-		ScreenFlip();			// 裏画面の内容を表画面に反映
+
+		ScreenFlip();	// 裏画面の内容を表画面に反映
+
 		fps.Wait();		//待機
 	}
+
+	// DXライブラリ使用の終了処理
+	DxLib_End(); 
+
 	return 0;
 }
