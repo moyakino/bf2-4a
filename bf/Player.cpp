3110b7eb3e7bcc430fce6afeeb9d99069bc5b18c
moyakino <104491816+moyakino@users.noreply.github.com>
2023-06-30 14:50:40 +0900

    Player 右左ボタン移動　Player画像の分割読み込み完了

--- bf/Player.cpp
+++ bf/Player.cpp
@@ -1,34 +1,86 @@
 #include "DxLib.h"
+#include "FPS.h"
 #include "Player.h"
 #include "PadInput.h"
 
+Fps fps;
+
 Player::Player()
 {
-	//タイトル画像データの読み込み
-	if ((D_PlayerImg = LoadGraph("images/REDApple.png")) == -1);
-	//L_Stick_X = PAD_INPUT::GetLStickX();
-	//L_Stick_Y = PAD_INPUT::GetLStickY();
+	//プレイヤー画像データの読み込み
+	if (D_PlayerImg[0] = LoadGraph("images/REDApple.png")) {}
+
+	if (LoadDivGraph("images/Player/Player_animation_d.png", 32, 8, 4, 64, 64, P_PlayerImg));
+	//if (D_PlayerImg[1] = LoadGraph("images/GOLDApple.png")) {}
+	//if (D_PlayerImg[2] = LoadGraph("images/BLUEApple.png")) {}
 	P_Move_X = 200;
 	P_Move_Y = 400;
+	P_MoveR_Flg = FALSE;
+	P_MoveL_Flg = FALSE;
+	P_Img = 0;
+
 }
 
 void Player::Update()
 {
-	if (32768 > PAD_INPUT::GetLStickX() && PAD_INPUT::GetLStickX() > 128) {
+	//右移動
+	if (32768 > PAD_INPUT::GetLStickX() && PAD_INPUT::GetLStickX() > 128 || PAD_INPUT::OnPressed(XINPUT_BUTTON_DPAD_RIGHT) == 1)  {
 		P_Move_X += 2;
+		P_MoveR_Flg = TRUE;
+		P_Img = Player::Animation(P_MoveR_Flg, P_MoveL_Flg);
+	}else {
+		P_MoveR_Flg = FALSE;
+		P_Img = Player::Animation(P_MoveR_Flg, P_MoveL_Flg);
 	}
 
-	if (-32769 < PAD_INPUT::GetLStickX() && PAD_INPUT::GetLStickX() < 128) {
+	//XINPUT_BUTTON_DPAD_LEFT
+
+	//左移動
+	if (-32769 < PAD_INPUT::GetLStickX() && PAD_INPUT::GetLStickX() < 128 || PAD_INPUT::OnPressed(XINPUT_BUTTON_DPAD_LEFT) == 1) {
 		P_Move_X -= 2;
+		P_MoveL_Flg = TRUE;
+		P_Img = Player::Animation(P_MoveR_Flg, P_MoveL_Flg);
+	}else {
+		P_MoveL_Flg = FALSE;
+		P_Img = Player::Animation(P_MoveR_Flg, P_MoveL_Flg);
 	}
 
+	//浮上
 	if (PAD_INPUT::OnButton(XINPUT_BUTTON_A) == 1) {
 		P_Move_Y -= 70;
 	}
 
+	// Aボタンが押されていない間 Y座標 400まで降下
 	if (PAD_INPUT::OnRelease(XINPUT_BUTTON_A) == 0 && P_Move_Y <= 400) {
 		P_Move_Y++;
 	}
+}
+
+int Player::Animation(int Anim1, int Anim2)
+{
+	int AnimImg = 0;
+
+	if (Anim1 == FALSE && Anim2 == FALSE) {
+		AnimImg = D_PlayerImg[0];
+	}
+	else if (Anim1 == TRUE) {
+		AnimImg = D_PlayerImg[1];
+	}
+	else if (Anim2 == TRUE) {
+		AnimImg = D_PlayerImg[2];
+	}
+
+	return AnimImg;
+}
+
+int Player::R_Player_MoveX()const
+{
+	return P_Move_X;
+}
+
+int Player::R_Player_MoveY()const
+{
+	return P_Move_Y;
 }
 
 void Player::Draw()const
@@ -38,5 +90,9 @@ void Player::Draw()const
 
 	DrawFormatString(0, 150, GetColor(255, 0, 0), " 左スティック：X座標 %d Y座標 %d", PAD_INPUT::GetLStickX(), PAD_INPUT::GetLStickY());
 
-	DrawRotaGraph(P_Move_X, P_Move_Y, 0.10, 0, D_PlayerImg, TRUE);
+	DrawFormatString(0, 200, GetColor(255, 0, 0), " 移動フラグ：左移動 %d 右移動 %d", P_MoveL_Flg, P_MoveR_Flg);
+
+	//DrawFormatString(300, 200, GetColor(255, 0, 0), " FPS：%.1f",fps.R_Fps());
+
+	DrawRotaGraph(P_Move_X, P_Move_Y, 1, 0, P_PlayerImg[0], TRUE);
 }
--- bf/Player.h
+++ bf/Player.h
@@ -5,13 +5,22 @@ class Player
 private:
 
 	//Aボタンの格納用変数
-	int L_Stick_X;
-	int L_Stick_Y;
+	//int L_Stick_X;
+	//int L_Stick_Y;
 	//Player x y 座標用変数
 	int P_Move_X;
 	int P_Move_Y;
 	//(仮)プレイヤー画像用変数
-	int D_PlayerImg;
+	int D_PlayerImg[3];
+
+	int P_PlayerImg[33];
+
+	int P_Img;
+
+	//(仮)プレイヤー右移動フラグ 移動無し:0 右移動:1
+	int P_MoveR_Flg;
+	//(仮)プレイヤー左移動フラグ 移動無し:0 右移動:1
+	int P_MoveL_Flg;
 
 public:
 	Player();
@@ -24,4 +33,11 @@ public:
 
 	//描画処理
 	void Draw() const;
+
+	//アニメーション
+	int Animation(int Anim1, int Anim2);
+
+	//Playerの X座標 Y座標 を戻り値として返す
+	int R_Player_MoveX()const;
+	int R_Player_MoveY()const;
 };
