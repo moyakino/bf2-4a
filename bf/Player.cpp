a5b16534272915f2fdaa045ac8e529a80fc4d1ae
moyakino <104491816+moyakino@users.noreply.github.com>
2023-07-10 18:01:59 +0900

    Playerのほぼ全ての動き

--- bf/Player.cpp
+++ bf/Player.cpp
@@ -5,23 +5,26 @@
 PLAYER::PLAYER()
 {
     //プレイヤー画像データの読み込み
-    if (LoadDivGraph("images/Player/Player_animation_d.png", 32, 8, 4, 64, 64, P_PlayerImg)){}
-    P_Img = P_PlayerImg[1];
+    if (LoadDivGraph("images/Player/Player_animation_d.png", 32, 8, 4, 64, 64, P_ArrayImg)){}
+    P_Img = 0;
     P_L_Stick = 0;
     P_Right_Btn = 0;
     P_Left_Btn = 0;
     P_A_Btn = 0;
+    P_A_Pressed = 0;
     P_Move_X = 100.0;
-    P_Move_Y = 370.0;
+    P_Move_Y = 350.0;
     P_Speed = 0.0;
+    P_AirSpeed = 0.0;
     P_MoveR_Flg = 0;
     P_MoveL_Flg = 0;
     P_Air_Flg = 0;
     P_Balloon_Flg = 1;
     P_TurnFlg = TRUE;
     P_FPS = 0;
-    P_Seconas = 0;
-
+    P_Seconas1 = 0;
+    MouseX = 0;
+    MouseY = 0;
 }
 
 void PLAYER::Update()
@@ -29,24 +32,36 @@ void PLAYER::Update()
     //フレーム取得
     P_FPS++;
 
+    //マウスから座標を読み取る
+    GetMousePoint(&MouseX, &MouseY);
+
     //左スティック 右ボタン 左ボタンの状態取得
     P_L_Stick = PAD_INPUT::GetLStickX();
     P_Right_Btn = PAD_INPUT::OnPressed(XINPUT_BUTTON_DPAD_RIGHT);
     P_Left_Btn = PAD_INPUT::OnPressed(XINPUT_BUTTON_DPAD_LEFT);
     P_A_Btn = PAD_INPUT::OnButton(XINPUT_BUTTON_A);
+    P_A_Pressed = PAD_INPUT::OnPressed(XINPUT_BUTTON_A);
+
+    if (P_Move_X <= -30) {
+        P_Move_X = 630;
+    }
+    else if (P_Move_X >= 630) {
+        P_Move_X = -30;
+    }
 
     //右移動
-    if (P_L_Stick > P_RIGHT_MOVE && P_Air_Flg == FALSE || P_Right_Btn == 1) {
+    if (P_L_Stick > RIGHT_MOVE || P_Right_Btn == 1) {
         P_MoveR_Flg = TRUE;
         P_TurnFlg = TRUE;
         //P_Move_X += 2;
-        P_Img = PLAYER::Run_Anim();
-        if (P_Speed <= 4.0f) {
-            P_Speed = P_Speed + 0.1f;
+        P_Img = Run_Anim();
+        //慣性
+        if (P_Speed <= 2.0f) {
+            P_Speed = P_Speed + 0.05f;
             P_Move_X = P_Move_X + P_Speed;
         }
-        else if(P_Speed >= 4.0f){
-            P_Speed = 4.0f;
+        else if(P_Speed >= 2.0f){
+            P_Speed = 2.0f;
             P_Move_X = P_Move_X + P_Speed;
         }
     }
@@ -54,18 +69,48 @@ void PLAYER::Update()
         P_MoveR_Flg = FALSE;
     }
 
+    //右空中移動
+    if (P_Air_Flg == TRUE && P_L_Stick > RIGHT_MOVE || P_Air_Flg == TRUE && P_Right_Btn == 1) {
+        P_TurnFlg = TRUE;
+        //慣性
+        if (P_AirSpeed <= 2.0f) {
+            P_AirSpeed = P_AirSpeed + 0.05f;
+            P_Move_X = P_Move_X + P_AirSpeed;
+        }
+        else if (P_AirSpeed >= 2.0f) {
+            P_AirSpeed = 2.0f;
+            P_Move_X = P_Move_X + P_AirSpeed;
+        }
+    }
+
+    //右浮上移動
+    if (P_A_Pressed == 1 && P_L_Stick > RIGHT_MOVE || P_A_Pressed == 1 && P_Right_Btn == 1) {
+        P_TurnFlg = TRUE;
+        P_Move_Y -= 0.5;
+        P_Img = Levitation_Anim2();
+        if (P_AirSpeed <= 3.0f) {
+            P_AirSpeed = P_AirSpeed + 0.05f;
+            P_Move_X = P_Move_X + P_AirSpeed;
+        }
+        else if (P_AirSpeed >= 3.0f) {
+            P_AirSpeed = 3.0f;
+            P_Move_X = P_Move_X + P_AirSpeed;
+        }
+    }
+
     //左移動
-    if (P_L_Stick < P_LEFT_MOVE && P_Air_Flg == FALSE || P_Left_Btn == 1) {
+    if (P_L_Stick < LEFT_MOVE || P_Left_Btn == 1) {
         P_MoveL_Flg = TRUE;
         P_TurnFlg = FALSE;
         //P_Move_X -= 2;
-        P_Img = PLAYER::Run_Anim();
-        if (P_Speed >= -4.0f) {
-            P_Speed = P_Speed + -0.1f;
+        P_Img = Run_Anim();
+        //慣性
+        if (P_Speed >= -2.0f) {
+            P_Speed = P_Speed + -0.05f;
             P_Move_X = P_Move_X + P_Speed;
         }
-        else if (P_Speed <= -4.0f) {
-            P_Speed = -4.0f;
+        else if (P_Speed <= -2.0f) {
+            P_Speed = -2.0f;
             P_Move_X = P_Move_X + P_Speed;
         }
     }
@@ -73,21 +118,58 @@ void PLAYER::Update()
         P_MoveL_Flg = FALSE;
     }
 
-    //待機中
-    if (P_MoveL_Flg == FALSE && P_MoveR_Flg == FALSE && P_Air_Flg == FALSE) {
-        P_Img = PLAYER::Stand_by_Anim();
-        P_Speed = P_Speed * 0.95f;
-        P_Move_X = P_Move_X + P_Speed;
+    //左空中移動
+    if (P_Air_Flg == TRUE && P_L_Stick < LEFT_MOVE || P_Air_Flg == TRUE && P_Left_Btn == 1) {
+        P_TurnFlg = FALSE;
+        //慣性
+        if (P_AirSpeed >= -2.0f) {
+            P_AirSpeed = P_AirSpeed + -0.05f;
+            P_Move_X = P_Move_X + P_AirSpeed;
+        }
+        else if (P_AirSpeed <= -2.0f) {
+            P_AirSpeed = -2.0f;
+            P_Move_X = P_Move_X + P_AirSpeed;
+        }
+    }
+
+    //左浮上移動
+    if (P_A_Pressed == 1 && P_L_Stick < LEFT_MOVE || P_A_Pressed == 1 && P_Left_Btn == 1) {
+        P_TurnFlg = FALSE;
+        P_Move_Y -= 0.5;
+        P_Img = Levitation_Anim2();
+        if (P_AirSpeed >= -3.0f) {
+            P_AirSpeed = P_AirSpeed + -0.05f;
+            P_Move_X = P_Move_X + P_AirSpeed;
+        }
+        else if (P_AirSpeed <= -3.0f) {
+            P_AirSpeed = -3.0f;
+            P_Move_X = P_Move_X + P_AirSpeed;
+        }
     }
     
-    //浮上
+
+    //待機中
+    if (P_MoveL_Flg == FALSE && P_MoveR_Flg == FALSE && P_Air_Flg == FALSE) {
+        P_Img = Stand_by_Anim();
+        //慣性
+        P_Speed = P_Speed * 0.97f;
+        P_Move_X = P_Move_X + P_Speed;
+    }
+
+    //浮上 ただ浮くだけ
     if (P_A_Btn == 1) {
-        P_Move_Y -= 20;
+        //インターバルをとる
+        if (P_FPS % 3 == 0 || P_FPS % 3 == 1 || P_FPS % 3 == 2 || P_FPS % 3 == 3) {
+            P_Img = Levitation_Anim1();
+            P_Move_Y -= 10;
+        }
     }
     else {
         //重力
         if (P_A_Btn == 0 && P_Move_Y <= 350.0) {
             P_Move_Y += 0.3;
+            P_Speed = 0;
+            P_Img = Levitation_Anim2();
             //P_TurnFlg = P_Move_Flg();
             P_Air_Flg = TRUE;
         }
@@ -99,29 +181,29 @@ void PLAYER::Update()
     //60fps == 1秒　で超えたら fpsを 0 にする
     if (P_FPS > 60) {
         P_FPS = 0;
-        P_Seconas++;
+        P_Seconas1++;
     }// P_FPS_INC は 秒数を取っている
-    else if (P_Seconas > 3) {
-        P_Seconas = 0;
-    }
+    else if (P_Seconas1 > 3) {
+        P_Seconas1 = 0;
+    }  
 }
 
 int PLAYER::Stand_by_Anim()
 {
     int S_AnimImg = 0;
 
-    //プレイヤーの画像の切り替えは秒数にしている フレームだと点滅する
-    if (P_Seconas == 0) {
-        S_AnimImg = P_PlayerImg[P_STAND_BY_BL2_ANIM_1];
+    // 0 から 3 秒
+    if (P_Seconas1 == 0) {
+        S_AnimImg = P_ArrayImg[STAND_BY_BALLOON2_1];
     }
-    else if (P_Seconas > 0 && P_Seconas < 2) {
-        S_AnimImg = P_PlayerImg[P_STAND_BY_BL2_ANIM_0];
+    else if (P_Seconas1 > 0 && P_Seconas1 < 2) {
+        S_AnimImg = P_ArrayImg[STAND_BY_BALLOON2_0];
     }
-    else if (P_Seconas > 1 && P_Seconas < 3) {
-        S_AnimImg = P_PlayerImg[P_STAND_BY_BL2_ANIM_1];
+    else if (P_Seconas1 > 1 && P_Seconas1 < 3) {
+        S_AnimImg = P_ArrayImg[STAND_BY_BALLOON2_1];
     }
-    else if (P_Seconas > 2 && P_Seconas < 4) {
-        S_AnimImg = P_PlayerImg[P_STAND_BY_BL2_ANIM_2];
+    else if (P_Seconas1 > 2 && P_Seconas1 < 4) {
+        S_AnimImg = P_ArrayImg[STAND_BY_BALLOON2_2];
     }
 
     return S_AnimImg;
@@ -131,20 +213,56 @@ int PLAYER::Run_Anim()
 {
     int R_AnimImg = 0;
 
+    // 5フレーム
     if (P_FPS % 20 == 0 || P_FPS % 20 == 1 || P_FPS % 20 == 2 || P_FPS % 20 == 3 || P_FPS % 20 == 4) {
-        R_AnimImg = P_PlayerImg[P_RUNNING_ANIM_0];
+        R_AnimImg = P_ArrayImg[RUN_BALLOON2_0];
     }
     else if (P_FPS % 20 == 5 || P_FPS % 20 == 6 || P_FPS % 20 == 7 || P_FPS % 20 == 8 || P_FPS % 20 == 9) {
-        R_AnimImg = P_PlayerImg[P_RUNNING_ANIM_1];
+        R_AnimImg = P_ArrayImg[RUN_BALLOON2_1];
     }
     else if (P_FPS % 20 == 10 || P_FPS % 20 == 11 || P_FPS % 20 == 12 || P_FPS % 20 == 13 || P_FPS % 20 == 14) {
-        R_AnimImg = P_PlayerImg[P_RUNNING_ANIM_2];
+        R_AnimImg = P_ArrayImg[RUN_BALLOON2_2];
     }
     else if (P_FPS % 20 == 15 || P_FPS % 20 == 16 || P_FPS % 20 == 17 || P_FPS % 20 == 18 || P_FPS % 20 == 19) {
-        R_AnimImg = P_PlayerImg[P_RUNNING_ANIM_3];
+        R_AnimImg = P_ArrayImg[RUN_BALLOON2_3];
     }
 
     return R_AnimImg;
+}
+
+int PLAYER::Levitation_Anim1()
+{
+    int F1_AnimImg = 0;
+
+    // 5フレーム
+    if (P_FPS % 15 == 0 || P_FPS % 15 == 1 || P_FPS % 15 == 2 || P_FPS % 15 == 3 || P_FPS % 15 == 4) {
+        F1_AnimImg = P_ArrayImg[LEVITATION_BALLOON2_0];
+    }
+    else if (P_FPS % 15 == 5 || P_FPS % 15 == 6 || P_FPS % 15 == 7 || P_FPS % 15 == 8 || P_FPS % 15 == 9) {
+        F1_AnimImg = P_ArrayImg[LEVITATION_BALLOON2_1];
+    }
+    else if (P_FPS % 15 == 10 || P_FPS % 15 == 11 || P_FPS % 15 == 12 || P_FPS % 15 == 13 || P_FPS % 15 == 14) {
+        F1_AnimImg = P_ArrayImg[LEVITATION_BALLOON2_2];
+    }
+
+    return F1_AnimImg;
+}
+
+int PLAYER::Levitation_Anim2()
+{
+    int F2_AnimImg = 0;
+
+    if (P_FPS % 15 == 0 || P_FPS % 15 == 1 || P_FPS % 15 == 2 || P_FPS % 15 == 3 || P_FPS % 15 == 4) {
+        F2_AnimImg = P_ArrayImg[LEVITATION_BALLOON2_3];
+    }
+    else if (P_FPS % 15 == 5 || P_FPS % 15 == 6 || P_FPS % 15 == 7 || P_FPS % 15 == 8 || P_FPS % 15 == 9) {
+        F2_AnimImg = P_ArrayImg[LEVITATION_BALLOON2_2];
+    }
+    else if (P_FPS % 15 == 10 || P_FPS % 15 == 11 || P_FPS % 15 == 12 || P_FPS % 15 == 13 || P_FPS % 15 == 14) {
+        F2_AnimImg = P_ArrayImg[LEVITATION_BALLOON2_4];
+    }
+
+    return F2_AnimImg;
 }
 
 PLAYER::~PLAYER()
@@ -163,7 +281,15 @@ void PLAYER::Draw()const
 
     DrawFormatString(0, 160, GetColor(255, 255, 255), " 移動フラグ：左移動 %d 右移動 %d", P_MoveL_Flg, P_MoveR_Flg);
 
+    DrawFormatString(0, 180, GetColor(255, 255, 255), " マウス座標：X座標 %d Y座標 %d", MouseX, MouseY);
+
     DrawFormatString(330, 120, GetColor(255, 255, 255), " FPS：%d", P_FPS);
+
+    //プレイヤーの当たり判定
+    DrawBox(P_Move_X + 20, P_Move_Y + 37, P_Move_X + 47, P_Move_Y + 65, GetColor(255, 255, 255), FALSE);
+
+    //風船の当たり判定
+    DrawBox(P_Move_X + 5, P_Move_Y + 10, P_Move_X + 59, P_Move_Y + 37, GetColor(255, 255, 255), FALSE);
 
     // TurnFlag: 画像の左右反転を行うかのフラグ
     //DrawRotaGraph(P_Move_X, P_Move_Y, 1, 0, P_Img, TRUE, P_TurnFlg);
--- bf/Player.h
+++ bf/Player.h
@@ -1,38 +1,63 @@
 #pragma once
-//プレイヤーの待機中アニメーション (風船が二つの場合)
-#define P_STAND_BY_BL2_ANIM_0 0
-#define P_STAND_BY_BL2_ANIM_1 1
-#define P_STAND_BY_BL2_ANIM_2 2
-//プレイヤーの待機中アニメーション (風船が一つの場合)
-#define P_STAND_BY_BL1_ANIM_0 3
-#define P_STAND_BY_BL1_ANIM_1 4
-#define P_STAND_BY_BL1_ANIM_2 5
-//プレイヤーの X軸方向の移動アニメーション
-#define P_RUNNING_ANIM_0 8
-#define P_RUNNING_ANIM_1 9
-#define P_RUNNING_ANIM_2 10
-#define P_RUNNING_ANIM_3 11
-//プレイヤーの Y軸方向の浮上アニメーション
-#define P_FLY_TO_THE_SKY_ANIM_0 16
-#define P_FLY_TO_THE_SKY_ANIM_1 17
-#define P_FLY_TO_THE_SKY_ANIM_2 18
-#define P_FLY_TO_THE_SKY_ANIM_3 19
-#define P_FLY_TO_THE_SKY_ANIM_4 20
+//プレイヤー 待機中アニメーション (風船が二つの場合)
+#define STAND_BY_BALLOON2_0 0
+#define STAND_BY_BALLOON2_1 1
+#define STAND_BY_BALLOON2_2 2
+
+//プレイヤー 待機中アニメーション (風船が一つの場合)
+#define STAND_BY_BALLOON1_0 3
+#define STAND_BY_BALLOON1_1 4
+#define STAND_BY_BALLOON1_2 5
+
+//プレイヤー X軸方向の移動アニメーション (風船が二つの場合)
+#define RUN_BALLOON2_0 8
+#define RUN_BALLOON2_1 9
+#define RUN_BALLOON2_2 10
+#define RUN_BALLOON2_3 11
+
+//プレイヤー X軸方向の移動アニメーション (風船が一つの場合)
+#define RUN_BALLOON1_0 12
+#define RUN_BALLOON1_1 13
+#define RUN_BALLOON1_2 14
+#define RUN_BALLOON1_3 15
+
+//プレイヤー Y軸方向の浮上アニメーション (風船が二つの場合)
+#define LEVITATION_BALLOON2_0 16
+#define LEVITATION_BALLOON2_1 17
+#define LEVITATION_BALLOON2_2 18
+#define LEVITATION_BALLOON2_3 19
+#define LEVITATION_BALLOON2_4 20
+
+//プレイヤー Y軸方向の浮上アニメーション (風船が一つの場合)
+#define LEVITATION_BALLOON1_0 24
+#define LEVITATION_BALLOON1_1 25
+#define LEVITATION_BALLOON1_2 26
+#define LEVITATION_BALLOON1_3 27
+#define LEVITATION_BALLOON1_4 28
+
+//プレイヤー 落下アニメーション
+#define FALL_0 21
+#define FALL_1 22
+#define FALL_2 23
+
+//プレイヤー 雷やられアニメーション
+#define THUNDER_0 29
+#define THUNDER_1 30
 
 //スティックの傾き具合からプレイヤーの状態を取得
-#define P_LEFT_MOVE -2000
-#define P_RIGHT_MOVE 2000
+#define LEFT_MOVE -2000
+#define RIGHT_MOVE 2000
 
 //プレイヤーの走るスピード
-#define P_RUN_SPEED 1
-#define P_SPEED_UP  0.2
-#define P_SPEED_UP1 0.2
+#define RUN_SPEED 1
+#define SPEED_UP  0.2
+#define SPEED_UP1 0.2
 
 class PLAYER
 {
 private:
 	//(仮)プレイヤー画像 分割読み込み用変数
-	int		P_PlayerImg[33];
+	int		P_ArrayImg[33];
 
 	//(仮)プレイヤー画像 描画用変数
 	int		P_Img;
@@ -46,13 +71,17 @@ private:
 	int		P_Left_Btn;
 	//(仮)Aボタン
 	int		P_A_Btn;
+	//(仮)Aボタン長押し
+	int		P_A_Pressed;
 
 	//Player  X座標用変数
-	float		P_Move_X;
+	float	P_Move_X;
 	//Player  Y座標用変数
 	float	P_Move_Y;
-	//プレイヤーのスピード
+	//プレイヤーの地上スピード
 	float	P_Speed;
+	//プレイヤーの空中スピード
+	float	P_AirSpeed;
 
 	//(仮)プレイヤー右移動フラグ 移動無し:0 右移動:1
 	int		P_MoveR_Flg;
@@ -67,8 +96,10 @@ private:
 
 	//FPSと秒数カウント
 	int		P_FPS;
-	int		P_Seconas;
+	int		P_Seconas1;
 	
+	int MouseX;
+	int MouseY;
 
 public:
 	PLAYER();
@@ -84,9 +115,12 @@ public:
 	
 	//プレイヤーの右移動処理
 	//int P_Move_Flg();
+
 	//Playerの待機状態中のアニメーション
 	int Stand_by_Anim();
 	//Playerが走っている時のアニメーション
 	int Run_Anim();
-	
+	//Playerが浮上している時のアニメーション 
+	int Levitation_Anim1();
+	int Levitation_Anim2();
 };
