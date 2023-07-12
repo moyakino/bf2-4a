de2a0cab438fc8399eb5240f6f735fa544324964
matsutake-taketo <taketo-matsutake-k22h@stu.kbc.ac.jp>
2023-07-12 14:09:33 +0900

    タイトル画面画像２

--- bf/TitleScene.cpp
+++ bf/TitleScene.cpp
@@ -17,7 +17,7 @@ Title::Title()
 {
 	if ((g_TitleImage = LoadGraph("images/Title/Title_Logo.png")) == -1);
 	if ((g_TitleImage2 = LoadGraph("images/Title/Title_ModeSelect.png")) == -1);
-
+	if ((g_TitleImage3 = LoadGraph("images/Title/Title_Credit.png")) == -1);
 }
 
 Title::~Title()
@@ -30,6 +30,7 @@ void Title::Draw()const
 	
 	DrawGraph(25, 0, g_TitleImage, TRUE);
 	DrawGraph(150, 300, g_TitleImage2, TRUE);
+	DrawGraph(150, 450, g_TitleImage3, TRUE);
 	DrawFormatString(0, 50, GetColor(255, 0, 0), "Title");
 	
 }
