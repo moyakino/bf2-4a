23fe6c48260b12ba116cc49885d99c46c089157a
Kotoribako <youto-tome-k22h@stu.kbc.ac.jp>
2023-07-21 12:09:44 +0900

    雷

--- bf/Thunder.cpp
+++ bf/Thunder.cpp
@@ -4,10 +4,18 @@
 
 Thunder::Thunder()
 {
-	//雷画像データの読み込み
-	if (LoadDivGraph("images/Stage/Stage_ThunderEffectAnimation.png", 3, 3, 1, 64, 64, S_ThunderImg)) {}
+	//雷(稲光)画像データの分割読み込み
+	if (LoadDivGraph("images/Stage/Stage_ThunderAnimation.png", 6, 6, 1, 64, 64, ThunderImg)) {}
 
-	S_Img = 0;
+	//雷(雷の弾)画像データの分割読み込み
+	if (LoadDivGraph("images/Stage/Stage_ThunderEffectAnimation.png", 3, 3, 1, 32, 32, EffectImg)) {}
+
+	ThunderImg[6];
+	T_Img = 0;
+
+	EffectImg[3];
+	E_Img = 0;
+
 	S_FPS = 0;
 	S_Seconas1 = 0;
 }
@@ -19,7 +27,7 @@ Thunder::~Thunder()
 
 void Thunder::Update()
 {
-	if (S_FPS > 30) {
+	if (S_FPS > 29) {
 		S_FPS = 0;
 		S_Seconas1++;
 	}// P_FPS_INC は 秒数を取っている
@@ -27,7 +35,8 @@ void Thunder::Update()
 		S_Seconas1 = 0;
 	}
 
-	S_Img = Thunder_Anim();
+	T_Img = Thunder_Anim();
+	E_Img = Effect_Anim();
 }
 
 int Thunder::Thunder_Anim()
@@ -36,20 +45,51 @@ int Thunder::Thunder_Anim()
 
 	// 0 から 3 秒
 	if (S_Seconas1 == 0) {
-		C_AnimImg = S_ThunderImg[THUNDER_ANIM1_0];
+		C_AnimImg = ThunderImg[THUNDER_ANIM1_0];
 	}
 	else if (S_Seconas1 > 0 && S_Seconas1 < 2) {
-		C_AnimImg = S_ThunderImg[THUNDER_ANIM1_0];
+		C_AnimImg = ThunderImg[THUNDER_ANIM1_1];
 	}
 	else if (S_Seconas1 > 1 && S_Seconas1 < 3) {
-		C_AnimImg = S_ThunderImg[THUNDER_ANIM1_0];
+		C_AnimImg = ThunderImg[THUNDER_ANIM1_2];
 	}
+	else if (S_Seconas1 > 0 && S_Seconas1 < 2) {
+		C_AnimImg = ThunderImg[THUNDER_ANIM1_3];
+	}
+	else if (S_Seconas1 > 1 && S_Seconas1 < 3) {
+		C_AnimImg = ThunderImg[THUNDER_ANIM1_4];
+	}
+	else if (S_Seconas1 > 0 && S_Seconas1 < 2) {
+		C_AnimImg = ThunderImg[THUNDER_ANIM1_5];
+	}
+	
 
 	return C_AnimImg;
+}
+
+int Thunder::Effect_Anim()
+{
+	int E_AnimImg = 0;
+
+	// 0 から 3 秒
+	if (S_Seconas1 == 0) {
+		E_AnimImg = EffectImg[THUNDER_EFFECT_ANIM1_0];
+	}
+	else if (S_Seconas1 > 0 && S_Seconas1 < 2) {
+		E_AnimImg = EffectImg[THUNDER_EFFECT_ANIM1_1];
+	}
+	else if (S_Seconas1 > 1 && S_Seconas1 < 3) {
+		E_AnimImg = EffectImg[THUNDER_EFFECT_ANIM1_2];
+	}
+
+	return E_AnimImg;
 }
 
 void Thunder::Draw() const
 {
 	//メニューカーソル（風船）の表示
-	DrawGraph(400, 200, S_Img, TRUE);
+	DrawGraph(400, 100, T_Img, TRUE);
+	
+	//メニューカーソル（風船）の表示
+	DrawGraph(400, 200, E_Img, TRUE);
 }
--- bf/Thunder.h
+++ bf/Thunder.h
@@ -3,14 +3,24 @@
 #define THUNDER_ANIM1_0 0
 #define THUNDER_ANIM1_1 1
 #define THUNDER_ANIM1_2 2
+#define THUNDER_ANIM1_3 3
+#define THUNDER_ANIM1_4 5
+#define THUNDER_ANIM1_5 6
+
+#define THUNDER_EFFECT_ANIM1_0 0
+#define THUNDER_EFFECT_ANIM1_1 1
+#define THUNDER_EFFECT_ANIM1_2 2
 
 class Thunder {
 
 private:
 
     //雷画像 分割読み込み用変数
-    int	S_ThunderImg[3];
-    int S_Img;
+    int	ThunderImg[6];
+    int T_Img;
+
+    int	EffectImg[3];
+    int E_Img;
 
     //FPSと秒数カウント
     int	S_FPS;
@@ -31,4 +41,5 @@ public:
     void Draw() const;
 
     int Thunder_Anim();
+    int Effect_Anim();
 };
