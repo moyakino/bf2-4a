5288d1eacbef3b2122c6ec61b225b489a70e3314
matsutake-taketo <120348208+matsutake-taketo@users.noreply.github.com>
2023-07-25 11:58:00 +0900

    fish anime 失敗

--- bf/Fish.cpp
+++ bf/Fish.cpp
@@ -4,8 +4,10 @@
 
 Fish::Fish()
 {
+    f_fps = 0;
+    F_Seconas1 = 0;
+
 	if (LoadDivGraph("images/Enemy/Enemy_FishAnimation.png", 10, 5, 2, 64, 64, Fish_ArrayImg)) {}
-	Fish_Img = Fish_ArrayImg[0];
 }
 
 Fish::~Fish()
@@ -15,17 +17,68 @@ Fish::~Fish()
 
 void Fish::Update()
 {
+    f_fps++;
 
+    if (f_fps > 59) {
+        f_fps = 0;
+        F_Seconas1++;
+
+    }
+    if (F_Seconas1 > 5) {
+        F_Seconas1 = 0;
+    }
 }
 
+int Fish::Fish_Anim()
+{
+
+
+    switch (F_Seconas1)
+    {
+    case 0:
+        F_AnimImg = Fish_ArrayImg[FISH_0];
+        break;
+    case 1:
+        F_AnimImg = Fish_ArrayImg[FISH_1];
+        break;
+    case 2:
+        F_AnimImg = Fish_ArrayImg[FISH_2];
+        break;
+    case 3:
+        F_AnimImg = Fish_ArrayImg[FISH_3];
+        break;
+    case 4:
+        F_AnimImg = Fish_ArrayImg[FISH_4];
+        break;
+    case 5:
+        F_AnimImg = Fish_ArrayImg[FISH_5];
+    }
+    return F_AnimImg;
+}
 void Fish::Draw() const
 {
-	DrawRotaGraph(300,410, 1.0f, 0, Fish_Img, TRUE);
-	/*DrawGraph(300, 700, Fish_Img, TRUE);*/
+    DrawFormatString(0, 20, 0xffffff, "Second:%d", F_Seconas1);
+    DrawFormatString(100, 400, 0xffffff, "f_fps:%d", f_fps);
+	DrawRotaGraph(300,410, 1.0f, 0, F_AnimImg, TRUE);
+	/*DrawGraph(300, 410, Fish_Img, TRUE);*/
 }
 
-//int Fish::Fish_Anim()
-//{
-//
-//	return 0;
-//}
+
+    //// 0 ‚©‚ç 3 •b
+    //if (F_Seconas1 == 0) {
+    //    F_AnimImg = Fish_ArrayImg[FISH_0];
+    //}
+    //else if (F_Seconas1 > 0 && F_Seconas1 < 2) {
+    //    F_AnimImg = Fish_ArrayImg[FISH_1];
+    //}
+    //else if (F_Seconas1 > 1 && F_Seconas1 < 3) {
+    //    F_AnimImg = Fish_ArrayImg[FISH_2];
+    //}
+    //else if (F_Seconas1 > 2 && F_Seconas1 < 4) {
+    //    F_AnimImg = Fish_ArrayImg[FISH_3];
+    //}
+    //else if (F_Seconas1 > 3 && F_Seconas1 < 5) {
+    //    F_AnimImg = Fish_ArrayImg[FISH_4];
+    //}
+
+   
--- bf/Fish.h
+++ bf/Fish.h
@@ -1,9 +1,20 @@
 #pragma once
+
+#define FISH_0 0
+#define FISH_1 1
+#define FISH_2 2
+#define FISH_3 3
+#define FISH_4 4
+#define FISH_5 5
+
 class Fish {
 private:
   int  Fish_ArrayImg[9];
-  int Fish_Img;
   int i = 0;
+  int	F_Seconas1;
+  int F_AnimImg = 0;
+  int Fish_Img;
+  int f_fps;
 
 public:
 
