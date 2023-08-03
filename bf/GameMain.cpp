392555889868ffc8322b20219f84af189a6a6f8d
minto121 <tomohide-matsuda-k22h@stu.kbc.ac.jp>
2023-08-03 13:08:13 +0900

    a

--- bf/GameMain.cpp
+++ bf/GameMain.cpp
@@ -39,7 +39,7 @@ AbstractScene* GameMain::Update()
 {
 	stage->Update();
 	player->Update();
-	//enemybird->Update(PLAYER::P_Move_X, PLAYER::P_Move_Y);
+	enemybird->Update(/*PLAYER::P_Move_X, PLAYER::P_Move_Y*/);
 	bubble->Update();
 	fish->Update();
 	enemybird->Update(/*player->GetLocationX(), player->GetLocationY()*/);
@@ -68,8 +68,7 @@ void GameMain::Draw()const
 	stage->Draw();
 	
 	fish->Draw();
-	//enemybird->Draw();
-	//enemybird->Draw();
+	enemybird->Draw();
 
 	bubble->Draw();
 
